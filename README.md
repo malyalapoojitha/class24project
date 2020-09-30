5b63f36dcc1887e3f1a93841e1ea582841fbf077
malyalapoojitha <70655679+malyalapoojitha@users.noreply.github.com>
2020-09-30 16:36:47 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# class24project
+# MatterJSBoilerPlate
+MatterJSBoilerPlate
---
+++ sketch.js
@@ -0,0 +1,44 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+const Render = Matter.Render;
+var dustbin;
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(1600, 700);
+	engine = Engine.create();
+	world = engine.world;
+	ground=new Ground(width/2,670,width,20);
+	paper=new Paper(100,650,40);
+	dustbin=new Dustbin(1200,650);
+	var render = Render.create({
+		 element: document.body,
+		  engine: engine, 
+		  options: { width: 1200, height: 700, wireframes: false } });
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  ground.display();
+  dustbin.display();
+paper.display();
+  drawSprites();
+ 
+}
+function keyPressed() {
+	if (keyCode === UP_ARROW) {
+
+	  Matter.Body.applyForce(paper.body,paper.body.position,{x:85,y:-85});
+  
+	}
+}
